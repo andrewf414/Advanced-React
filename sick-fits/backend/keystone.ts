10b5edabd0e2be8848e159ad6200fb9191713b37
Andrew <andrewhfitz@gmail.com>
2021-02-28 14:10:53 +1100

    keystone starting

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,47 @@
+import "dotenv/config";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from "@keystone-next/keystone/auth";
+import { User } from "./schemas/User";
+
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+    // TODO: add in initial roles
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      // TODO: add data seeding here
+    },
+    lists: createSchema({
+      // schema items go in here
+      User,
+    }),
+    ui: {
+      // TODO: change this for roles
+      isAccessAllowed: () => true,
+    },
+    // TODO: add session values here
+  })
+);
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,14 @@
+import Router from 'next/router';
+
+// swap with our own
+import NProgress from 'nprogress';
 import Page from '../components/Page';
+// import 'nprogress/nprogress.css'; default one
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
